9e52e6310f69d9bce323197a91fc861b543cd1a9
ralph950412 <ralph950412@gmail.com>
2025-02-04 16:21:21 +0800

    smartspace
    Change-Id: I1d14ea6ba100d0a5525c2e8f847b8dbf4d6d9b75

--- src/com/statix/android/systemui/dagger/SystemUIStatixModule.java
+++ src/com/statix/android/systemui/dagger/SystemUIStatixModule.java
@@ -34,6 +34,7 @@ import com.android.systemui.media.muteawait.MediaMuteAwaitConnectionCli;
 import com.android.systemui.media.nearby.NearbyMediaDevicesManager;
 import com.android.systemui.navigationbar.NavigationBarControllerModule;
 import com.android.systemui.navigationbar.gestural.GestureModule;
+import com.android.systemui.plugins.BcSmartspaceDataPlugin;
 import com.android.systemui.plugins.qs.QSFactory;
 import com.android.systemui.qs.dagger.QSModule;
 import com.android.systemui.reardisplay.RearDisplayModule;
@@ -71,6 +72,10 @@ import com.android.systemui.unfold.SysUIUnfoldStartableModule;
 import com.android.systemui.unfold.UnfoldTransitionModule;
 import com.android.systemui.volume.dagger.VolumeModule;
 import com.android.systemui.wallpapers.dagger.WallpaperModule;
+
+import com.google.android.systemui.smartspace.BcSmartspaceDataProvider;
+import com.google.android.systemui.smartspace.dagger.SmartspaceGoogleModule;
+import com.google.android.systemui.statusbar.NotificationLockscreenUserManagerGoogle;
 
 import com.statix.android.systemui.assist.StatixAssistManager;
 import com.statix.android.systemui.biometrics.FingerprintInteractiveToAuthProviderImpl;
@@ -131,6 +136,8 @@ import javax.inject.Named;
             SceneContainerFrameworkModule.class,
             ScreenDecorationsModule.class,
             ShadeModule.class,
+            SmartspaceGoogleModule.class,
+            StatixAssistModule.class,
             StatixStartCentralSurfacesModule.class,
             StatixPowerModule.class,
             StatixQSModule.class,
@@ -153,7 +160,7 @@ public abstract class SystemUIStatixModule {
 
     @Binds
     abstract NotificationLockscreenUserManager bindNotificationLockscreenUserManager(
-            NotificationLockscreenUserManagerImpl notificationLockscreenUserManager);
+            NotificationLockscreenUserManagerGoogle notificationLockscreenUserManager);
 
     @Provides
     @SysUISingleton
@@ -235,4 +242,10 @@ public abstract class SystemUIStatixModule {
 
     @Binds
     abstract AssistManager bindAssistManager(StatixAssistManager assistManager);
+
+    @Provides
+    @SysUISingleton
+    static BcSmartspaceDataPlugin provideBcSmartspaceDataPlugin() {
+        return new BcSmartspaceDataProvider();
+    }
 }
