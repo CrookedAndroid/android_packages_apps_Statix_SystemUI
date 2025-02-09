a46720b0e682da2a0caf53dcec3fb5c63984cf07
Ido Ben-Hur <idoybh2@gmail.com>
2025-02-09 16:52:46 -0500

    FlashlightStrengthTile: Switch to a listener
    This fixes a race that occurs when the camera characteristics aren't ready when SystemUI is.
    
    Change-Id: I8b18ddef313cb16c18d12dd1967228bc47115487

---
+++ src/com/statix/android/systemui/qs/tiles/FlashlightStrengthTile.java
@@ -0,0 +1,254 @@
+package com.statix.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraCharacteristics.Key;
+import android.hardware.camera2.CameraManager;
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.android.internal.logging.MetricsLogger;
+import com.statix.android.systemui.res.R;
+import com.android.systemui.animation.Expandable;
+import com.android.systemui.dagger.qualifiers.Background;
+import com.android.systemui.dagger.qualifiers.Main;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.plugins.FalsingManager;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.QsEventLogger;
+import com.android.systemui.qs.logging.QSLogger;
+import com.android.systemui.qs.tiles.FlashlightTile;
+import com.android.systemui.statusbar.policy.FlashlightController;
+
+import com.statix.android.systemui.qs.tileimpl.TouchableQSTile;
+
+import javax.inject.Inject;
+
+public class FlashlightStrengthTile extends FlashlightTile implements TouchableQSTile {
+
+    public static final String TILE_SPEC = "flashlight";
+
+    private static final Key<Integer> FLASHLIGHT_MAX_BRIGHTNESS_CHARACTERISTIC =
+            CameraCharacteristics.FLASH_INFO_STRENGTH_MAXIMUM_LEVEL;
+    private static final Key<Integer> FLASHLIGHT_DEFAULT_BRIGHTNESS_CHARACTERISTIC =
+            CameraCharacteristics.FLASH_INFO_STRENGTH_DEFAULT_LEVEL;
+
+    private static final String FLASHLIGHT_BRIGHTNESS_SETTING = "flashlight_brightness";
+
+    private final CameraManager mCameraManager;
+    private final FlashlightController mFlashlightController;
+    private boolean mSupportsSettingFlashLevel;
+    private int mMaxLevel;
+    private int mCurrentLevel;
+    private float mCurrentPercent;
+    private boolean mClicked = true;
+    private Looper mBgLooper;
+    private boolean mRegistered = false;
+
+    @Nullable private String mCameraId;
+
+    private final CameraManager.TorchCallback mTorchCallback =
+            new CameraManager.TorchCallback() {
+                @Override
+                public void onTorchStrengthLevelChanged(String cameraId, int newStrengthLevel) {
+                    if (!cameraId.equals(mCameraId)) {
+                        return;
+                    }
+                    // We don't wanna refresh state for same values as this callback
+                    // will be invoked from this tile as well.
+                    if (mCurrentLevel == newStrengthLevel) {
+                        return;
+                    }
+                    // Update current percent/level and refresh the tile.
+                    mCurrentLevel = newStrengthLevel;
+                    mCurrentPercent = ((float) mCurrentLevel) / ((float) mMaxLevel);
+                    Settings.System.putFloat(
+                            mContext.getContentResolver(),
+                            FLASHLIGHT_BRIGHTNESS_SETTING,
+                            mCurrentPercent);
+                    refreshState(true);
+                }
+            };
+
+    private final View.OnTouchListener mTouchListener =
+            new View.OnTouchListener() {
+                float initX = 0;
+                float initPct = 0;
+                boolean moved = false;
+
+                @Override
+                public boolean onTouch(View view, MotionEvent motionEvent) {
+                    if (!mSupportsSettingFlashLevel) return false;
+
+                    switch (motionEvent.getAction()) {
+                        case MotionEvent.ACTION_DOWN -> {
+                            initX = motionEvent.getX();
+                            initPct = initX / view.getWidth();
+                            mClicked = false;
+                            return true;
+                        }
+                        case MotionEvent.ACTION_MOVE -> {
+                            float newPct = motionEvent.getX() / view.getWidth();
+                            float deltaPct = Math.abs(newPct - initPct);
+                            if (deltaPct > .03f) {
+                                view.getParent().requestDisallowInterceptTouchEvent(true);
+                                moved = true;
+                                mCurrentPercent = Math.max(0.01f, Math.min(newPct, 1));
+                                Settings.System.putFloat(
+                                        mContext.getContentResolver(),
+                                        FLASHLIGHT_BRIGHTNESS_SETTING,
+                                        mCurrentPercent);
+                                handleClick(null);
+                            }
+                            return true;
+                        }
+                        case MotionEvent.ACTION_UP -> {
+                            if (moved) {
+                                moved = false;
+                                Settings.System.putFloat(
+                                        mContext.getContentResolver(),
+                                        FLASHLIGHT_BRIGHTNESS_SETTING,
+                                        mCurrentPercent);
+                            } else {
+                                mClicked = true;
+                                handleClick(null);
+                            }
+                            return true;
+                        }
+                    }
+                    return true;
+                }
+            };
+
+    @Inject
+    public FlashlightStrengthTile(
+            QSHost host,
+            QsEventLogger qsEventLogger,
+            @Background Looper backgroundLooper,
+            @Main Handler mainHandler,
+            FalsingManager falsingManager,
+            MetricsLogger metricsLogger,
+            StatusBarStateController statusBarStateController,
+            ActivityStarter activityStarter,
+            QSLogger qsLogger,
+            FlashlightController flashlightController) {
+        super(
+                host,
+                qsEventLogger,
+                backgroundLooper,
+                mainHandler,
+                falsingManager,
+                metricsLogger,
+                statusBarStateController,
+                activityStarter,
+                qsLogger,
+                flashlightController);
+        mCameraManager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
+        mFlashlightController = flashlightController;
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        if (!listening) {
+            if (mRegistered) {
+                mCameraManager.unregisterTorchCallback(mTorchCallback);
+                mRegistered = false;
+            }
+            return;
+        }
+        int mDefaultLevel;
+        try {
+            mCameraId = getCameraId();
+            CameraCharacteristics characteristics =
+                    mCameraManager.getCameraCharacteristics(mCameraId);
+            mSupportsSettingFlashLevel =
+                    mFlashlightController.isAvailable()
+                            && mCameraId != null
+                            && characteristics.get(FLASHLIGHT_MAX_BRIGHTNESS_CHARACTERISTIC) > 1;
+            mMaxLevel = (int) characteristics.get(FLASHLIGHT_MAX_BRIGHTNESS_CHARACTERISTIC);
+            mDefaultLevel = (int) characteristics.get(FLASHLIGHT_DEFAULT_BRIGHTNESS_CHARACTERISTIC);
+        } catch (CameraAccessException | NullPointerException e) {
+            Log.d("FlashlightStrengthTile", "Setting to non-controllable defaults");
+            mCameraId = null;
+            mSupportsSettingFlashLevel = false;
+            mMaxLevel = 1;
+            mDefaultLevel = 0;
+        }
+        float defaultPercent = ((float) mDefaultLevel) / ((float) mMaxLevel);
+        mCurrentPercent =
+                Settings.System.getFloat(
+                        mContext.getContentResolver(),
+                        FLASHLIGHT_BRIGHTNESS_SETTING,
+                        defaultPercent);
+        mCurrentLevel = (int) ((float) mMaxLevel * mCurrentPercent);
+        mCameraManager.registerTorchCallback(mTorchCallback, new Handler(mBgLooper));
+        mRegistered = true;
+    }
+
+    @Override
+    public View.OnTouchListener getTouchListener() {
+        return mSupportsSettingFlashLevel ? mTouchListener : null;
+    }
+
+    @Override
+    public String getSettingsSystemKey() {
+        return "flashlight_brightness";
+    }
+
+    @Override
+    protected void handleClick(@Nullable Expandable expandable) {
+        boolean newState = !mClicked || !mState.value;
+        if (mSupportsSettingFlashLevel && newState) {
+            try {
+                int level = (int) (mCurrentPercent * ((float) mMaxLevel));
+                if (level == 0) {
+                    mFlashlightController.setFlashlight(false);
+                    newState = false;
+                } else {
+                    mCameraManager.turnOnTorchWithStrengthLevel(mCameraId, level);
+                }
+            } catch (CameraAccessException e) {
+            }
+        } else {
+            mFlashlightController.setFlashlight(newState);
+        }
+        refreshState(newState);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        super.handleUpdateState(state, arg);
+        if (mSupportsSettingFlashLevel) {
+            state.label =
+                    String.format(
+                            "%s - %s%%",
+                            mHost.getContext().getString(R.string.quick_settings_flashlight_label),
+                            Math.round(mCurrentPercent * 100f));
+        }
+    }
+
+    private String getCameraId() throws CameraAccessException {
+        String[] ids = mCameraManager.getCameraIdList();
+        for (String id : ids) {
+            CameraCharacteristics c = mCameraManager.getCameraCharacteristics(id);
+            Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+            Integer lensFacing = c.get(CameraCharacteristics.LENS_FACING);
+            if (flashAvailable != null
+                    && flashAvailable
+                    && lensFacing != null
+                    && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                return id;
+            }
+        }
+        return null;
+    }
+}
